3834e39aebc5f1946c791e61dbbdd0d48a772c7f
sayaki007 <77444220+sayaki007@users.noreply.github.com>
2022-08-18 16:13:05 +0700

    upload terbaru

--- project-pelatihan/routes/web.php
+++ project-pelatihan/routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/book', 'App\Http\Controllers\BookController.php@index');
+Route::get('/book/create', 'App\Http\Controllers\BookController.php@create');
+Route::get('/book/edit', 'App\Http\Controllers\BookController.php@edit');
+Route::get('/book/update', 'App\Http\Controllers\BookController.php@update');
+Route::get('/book/delete', 'App\Http\Controllers\BookController.php@index');
+
 Route::get('/', [ItemController::class, 'index']);
 Route::get('/index2', 'App\Http\Controllers\ItemController@index2');
 Route::get('/item/form', 'App\Http\Controllers\ItemController@itemForm');
